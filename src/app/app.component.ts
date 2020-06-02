a156efcb298e279f5c95b8a29847e8dbce0d9f7a
vioswift <Kylewww@gmail.com>
2020-06-02 20:25:29 +1000

    Completed form, need to work on validation and formatting of website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'perm2contract';
+  title = 'Perm2Contract';
+  baseSalary: number = 120000;
+  super: number = 0.095;
+  workingDays: number = 230;
+  dailyRate: number = 1.3;
+
+  setWorkingDays(event: any){
+    this.workingDays = event.target.value;
+  }
+
+  setDailyRate(event: any){
+    this.dailyRate = event.target.value;
+  }
+
+  setSuper(event: any){
+    this.super = event.target.value;
+  }
+
+  setBaseSalary(event: any){
+    this.baseSalary = event.target.value;
+  }
+
+  getDailyRateAsPercent(){
+    return (this.dailyRate * 100) - 100;
+  }
+
+  getTotalPackage(){
+    return this.getTotalSuper() + this.baseSalary;
+  }
+
+  getTotalSuper(){
+    return this.baseSalary * this.super;
+  }
+
+  getTotalDividedAvgWorkingDays(){
+    return this.getTotalPackage() / this.workingDays;
+  }
+
+  getTotalDailyRate(){
+    return this.getTotalDividedAvgWorkingDays() * this.dailyRate;
+  }
 }
